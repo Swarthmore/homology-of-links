908d04ebc9a825c273b09e3fb13a18f01e349cb9
Anthony Weed <aweed1@swarthmore.edu>
2022-11-11 10:15:33 -0500

    Added comments

--- public/scripts/checkinput.js
+++ public/scripts/checkinput.js
@@ -1,26 +1,26 @@
 // This script disables the input dialog when conditions are unmet. IE. You have to input 3 to n digits to be able to run.
 function checkInput() {
-
+    // Get the dom elements 
     const button = document.getElementById('submit-btn')
     const input = document.getElementById('args-input')    
+    // Get the value from the input
     const split = input.value.split(' ').filter(n => n)
-    // If there are 3 or more numbers, enable the submit button
+    // Start with a valid state
     let isvalid = true 
+    // This checks if the input contains valid characters 
     split.forEach(n => {
-        // This checks if item is a number
         if (!/^\d*$/.test(+n)) {
             isvalid = false
         }
     })
-
+    // At least 3 numbers are required to process
     if (split.length < 3) {
         isvalid = false
     }
-
+    // Set the button state accordinly
     if (isvalid) {
         button.disabled = false
     } else {
         button.disabled = true
     }
-
 }
